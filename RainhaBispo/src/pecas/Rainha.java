0838f1dfc3c66958f8ef6b7ca9674304a79a0425
Srº Cascudo <Sr Cascudo@172.16.90.219>
2018-11-14 19:31:07 -0200

    Classe Rainha e UML alterados

--- RainhaBispo/src/pecas/Rainha.java
+++ RainhaBispo/src/pecas/Rainha.java
@@ -1,39 +1,105 @@
 package pecas;
 
-import tabuleiroHipotetico.Tabuleiro;
-
 public class Rainha extends Peca {
-	static Posicao antigaPosicao = null;
-	
-	
+	static private Posicao antigaPosicao = null;
+
 	@Override
 	public boolean mover(Posicao posicao) {
+		Peca restricoesRainha[][] = new Peca[8][8];
+		Rainha rainha = new Rainha();
+		restricoesRainha = tabuleiro.getMatrizPeca();
+
 		if (isNuncaMovimentado()) {
-			//antigaPosicao = (ATRIBUIR POSICAO INICIAL DA RAINHA)
-		}
+			// antigaPosicao = (ATRIBUIR POSICAO INICIAL DA RAINHA)
+		} 
 		else {
 			antigaPosicao = posicao;
 		}
 
-		if ((antigaPosicao.getLinha() + antigaPosicao.getColuna()) == (posicao.getLinha() + posicao.getColuna()) ) {
-			return true;
-			//MOVE A RAINHA NA DIAGONAL PRINCIPAL
+		if (restricoesRainha[posicao.getLinha()][posicao.getColuna()] instanceof Peca								//=======================================//
+				&& rainha.isCorPreta() != restricoesRainha[posicao.getLinha()][posicao.getColuna()].isCorPreta()) {	//=======================================//
+		}																											//=ESSE BLOCO É RESPONSAVEL POR GARANTIR=//
+		else {																										//=SE HA UMA PECA ADVERSARIA NA POSICAO =//
+			return false;																							//=======================================//
+		}																											//=======================================//
+		
+		for (int linha = 0; linha < restricoesRainha.length; linha++) {			//VARREDURA DE TODAS
+			for (int coluna = 0; coluna < restricoesRainha.length; coluna++) {	//AS POSICOES DA MATRIZ
+				
+				if (posicao.getLinha() + posicao.getColuna() == linha + coluna
+						|| antigaPosicao.getColuna() - antigaPosicao.getLinha() == coluna - linha) {//SELECIONA AS DIAGONAIS  
+					
+					if (posicao.getLinha() < linha) {//VERIFICA SE A RAINHA QUER SE MOVER NAS DIAGONAIS INFERIOR
+
+						if (restricoesRainha[linha][coluna] instanceof Peca) {//VERIFICA SE UMA PECA NO CAMINHO ATÉ A NOVA POSICAO PELAS DIAGONAIS INFERIOR
+							return false;
+						}	
+					}
+					if (posicao.getLinha() > linha) {//VERIFICA SE A RAINHA QUER SE MOVER NA DIAGONAIS SUPERIOR
+		
+						if (restricoesRainha[linha][coluna] instanceof Peca) {//VERIFICA SE UMA PECA NO CAMINHO ATÉ A NOVA POSICAO PELAS DIAGONAIS SUPERIOR
+							return false;
+						}
+					}
+				}
+			
+				//HORIZONTAIS
+				if (posicao.getColuna() < coluna) {
+					if (restricoesRainha[linha][coluna] instanceof Peca) {//VERIFICA SE UMA PECA NO CAMINHO ATÉ A NOVA POSICAO PELA HORIZONTAL A ESQUEDA
+						return false;
+					}	
+				}
+				if (posicao.getColuna() > coluna) {
+					if (restricoesRainha[linha][coluna] instanceof Peca) {//VERIFICA SE UMA PECA NO CAMINHO ATÉ A NOVA POSICAO PELA HORIZONTAL A DIREITA
+						return false;
+					}	
+				}
+				
+				//VERTICAIS
+				if (posicao.getLinha() < linha) {
+					if (restricoesRainha[linha][coluna] instanceof Peca) {//VERIFICA SE UMA PECA NO CAMINHO ATÉ A NOVA POSICAO PELA VERTICAL INFERIOR;
+						return false;
+					}
+				}
+				
+				if (posicao.getLinha() > linha) {
+					if (restricoesRainha[linha][coluna] instanceof Peca) {//VERIFICA SE UMA PECA NO CAMINHO ATÉ A NOVA POSICAO PELA VERTICAL SUPERIOR;
+						return false;
+					}
+				}
+			}
 		}
 		
+		
+		
+		if ((antigaPosicao.getLinha() + antigaPosicao.getColuna()) == (posicao.getLinha() + posicao.getColuna())) {
+			restricoesRainha[posicao.getLinha()][posicao.getColuna()] = rainha;
+			tabuleiro.setMatrizPeca(restricoesRainha);//ATUALIZA A POSICAO DA RAINHA NO TABULEIRO
+			return true;
+			// MOVE A RAINHA NA DIAGONAL PRINCIPAL
+		}
+
 		if ((antigaPosicao.getColuna() - antigaPosicao.getLinha()) == (posicao.getColuna() - posicao.getLinha())) {
+			restricoesRainha[posicao.getLinha()][posicao.getColuna()] = rainha;
+			tabuleiro.setMatrizPeca(restricoesRainha);//ATUALIZA A POSICAO DA RAINHA NO TABULEIRO
 			return true;
-			//MOVE A RAINHA NA DIAGONAL SECUNDARIA
+			// MOVE A RAINHA NA DIAGONAL SECUNDARIA
 		}
-		
+
 		if (antigaPosicao.getColuna() == posicao.getColuna()) {
+			restricoesRainha[posicao.getLinha()][posicao.getColuna()] = rainha;
+			tabuleiro.setMatrizPeca(restricoesRainha);//ATUALIZA A POSICAO DA RAINHA NO TABULEIRO
 			return true;
-			//MOVE RAINHA NA HORIZONTAL
+			// MOVE RAINHA NA HORIZONTAL
 		}
-		
-		if (antigaPosicao.getLinha() == antigaPosicao.getLinha()) {
+
+		if (antigaPosicao.getLinha() == posicao.getLinha()) {
+			restricoesRainha[posicao.getLinha()][posicao.getColuna()] = rainha;
+			tabuleiro.setMatrizPeca(restricoesRainha);//ATUALIZA A POSICAO DA RAINHA NO TABULEIRO
 			return true;
-			//MOVE RAINHA NA VERTICAL
+			// MOVE RAINHA NA VERTICAL
 		}
+
 		return false;
 	}
 }
